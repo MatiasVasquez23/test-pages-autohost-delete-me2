7d4ee710428d5c65b8761b509cd311dc5649f161
MatiasVasquez23 <m.vsquezgatica@uandresbello.edu>
2026-09-04 01:31:09 -0400

    Add script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+let count = 0; const counterDisplay = document.getElementById('counter'); const incrementBtn = document.getElementById('incrementBtn'); incrementBtn.addEventListener('click', () => { count++; counterDisplay.textContent = count; });
